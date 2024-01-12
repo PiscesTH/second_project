0b2d33402f289b638ce49f3df23af3b6de30c7fc
TH <kareme007@gmail.com>
2024-01-12 18:07:47 +0900

    24.01.12.

---
+++ src/main/java/com/green/second_project/security/JwtTokenProvider.java
@@ -0,0 +1,11 @@
+package com.green.second_project.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider  {
+}
---
+++ src/main/java/com/green/second_project/user/model/UserSignInDto.java
@@ -0,0 +1,9 @@
+package com.green.second_project.user.model;
+
+import lombok.Data;
+
+@Data
+public class UserSignInDto {
+    private String uid;
+    private String upw;
+}
